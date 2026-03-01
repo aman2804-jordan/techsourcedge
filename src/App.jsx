77151dd039dca3ee8597937df1967d225934c774
aman2804-jordan <tiwari113@gmail.com>
2026-03-01 20:09:36 +0530

    latest update for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
   return () => document.removeEventListener("keydown", blockKeys);
 }, []);
 
-
+console.log("TEST DEPLOY CHECK");
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
