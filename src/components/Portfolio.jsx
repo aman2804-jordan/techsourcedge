078874f27319dea8354a8dc4f4cfeaa5c0031d3a
aman2804-jordan <tiwari113@gmail.com>
2026-03-07 01:29:15 +0530

    size

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -196,7 +196,7 @@ technologies: ["React", "Node.js", "Express.js", "MongoDB", "REST API"],
         <img 
           src={project.image} 
           alt={project.title}
-          className="w-full h-56 md:h-72 object-contain bg-gray-100"  /* Changed from h-64 */
+          className="w-full h-56 md:h-92 object-cover"  /* Changed from h-64 */
         />
       </div>
 
