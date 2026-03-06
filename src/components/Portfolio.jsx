4690e2e8c6946197d5a0706e913f32d3d05439b4
aman2804-jordan <tiwari113@gmail.com>
2026-03-07 02:07:38 +0530

    image

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ChevronLeft, ChevronRight, ExternalLink, Github, Calendar, User, ArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import kalamImage from "../assets/kalamaejazbaaatdp";
+import kalamImage from "../assets/kalamaejazbaaatdp.jpeg";
 
 
 export default function Portfolio() {
