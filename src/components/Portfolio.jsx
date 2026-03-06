f126b07b97bd3f01d85ae7a4adc02ab54de1f538
aman2804-jordan <tiwari113@gmail.com>
2026-03-07 01:26:36 +0530

    image crop

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -196,7 +196,7 @@ technologies: ["React", "Node.js", "Express.js", "MongoDB", "REST API"],
         <img 
           src={project.image} 
           alt={project.title}
-          className="w-full h-48 md:h-full object-cover"  /* Changed from h-64 */
+          className="w-full h-56 md:h-72 object-contain bg-gray-100"  /* Changed from h-64 */
         />
       </div>
 
